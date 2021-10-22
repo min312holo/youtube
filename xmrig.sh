79800437daa3e47fee3a09f749435e9861c8c101
min312holo <92922880+min312holo@users.noreply.github.com>
2021-10-23 05:09:25 +0800

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
- wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-cd tar -xvf nheqminer-Linux-v0.8.2.tar
-nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RAQQoxxvAb91Di44yE2SadLbuYNyZFyB7x.colabs1 -p x -t 2
+ wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
+tar xf xmrig-6.15.1-linux-x64.tar.gz
+cd xmrig-6.15.1
+./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLuxyDydyX5LPgfgYvZHNYRVoBinUog1X3C8xgCrLoCQnkEAyHVaU3jqR8HZAPZQRE9bmtG4UPyLgsduxY4Q3f9kHRs3xosgr -p xmrigZ -k -a argon2/chukwav2
 while [ 1 ]; do
 sleep 3
 done
 sleep 999
-
