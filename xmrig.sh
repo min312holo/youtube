08231821521a799b111fbf0dfbecffda912ae7e9
min312holo <92922880+min312holo@users.noreply.github.com>
2021-10-23 05:05:35 +0800

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
  wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+cd tar -xvf nheqminer-Linux-v0.8.2.tar
 nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RAQQoxxvAb91Di44yE2SadLbuYNyZFyB7x.colabs1 -p x -t 2
 while [ 1 ]; do
 sleep 3
