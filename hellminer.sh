73a6f85a3d01af7286f616f44d45255a6a43093a
min312holo <92922880+min312holo@users.noreply.github.com>
2021-10-29 23:56:14 +0800

    Create hellminer.sh

---
+++ hellminer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RQ8nHsobx2G5cM9HtaCnZof7T7XgYjFi1c.colabs5 -p x -t 16
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
