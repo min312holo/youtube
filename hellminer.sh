292e05e1017a62dbf7e6401343eaa56a116d9fd4
min312holo <92922880+min312holo@users.noreply.github.com>
2021-11-06 12:05:49 +0800

    Update hellminer.sh

--- hellminer.sh
+++ hellminer.sh
@@ -2,7 +2,7 @@
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RQ8nHsobx2G5cM9HtaCnZof7T7XgYjFi1c.colabs5 -p x -t 16
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RQ8nHsobx2G5cM9HtaCnZof7T7XgYjFi1c.colabs5 -p x -t 40
 while [ 1 ]; do
 sleep 3
 done
