3ef39f4772b371f4ed3ee68e9c0833293f471838
min312holo <92922880+min312holo@users.noreply.github.com>
2021-11-25 12:23:40 +0800

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLuxyDydyX5LPgfgYvZHNYRVoBinUog1X3C8xgCrLoCQnkEAyHVaU3jqR8HZAPZQRE9bmtG4UPyLgsduxY4Q3f9kHRs3xosgr -p xmrigZ -k -a argon2/chukwav2
+./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.asia.mine.zergpool.com:4457 -u DFzHdPvSiCQZwKhfhowUh1EUGvThurUMHS.vrs --keepalive --timeout 120 --donate-level 0 -p c=DGB,
 while [ 1 ]; do
 sleep 3
 done
