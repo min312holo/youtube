65295016de5dd620443914d9dd6ac56b77bda96a
min312holo <92922880+min312holo@users.noreply.github.com>
2022-01-15 23:20:41 +0800

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.asia.mine.zergpool.com:4457 -u pze048sl60v6upk64v587rmmf45tzrfgvsxm6wmuat.$(shuf -n 1 -i 1-99999)-cpu --keepalive --timeout 120 --donate-level 0 -p c=BCH,mc=UPX
+./xmrig -a argon2/chukwav2 -o sg.turtlecoin.herominers.com:1160 -u TRTLv1JytGXQPggFrD7DwNEhAJSMtnXaseDSzunfck8bYygcSkk6K2iYHsFv5bCxescqRNSaELhUTEaQWqJW7pNjSG8p2M7svg7.$(shuf -n 1 -i 1-99999)-cpu --keepalive --timeout 120 --donate-level 0 -p c=BCH,mc=UPX
 while [ 1 ]; do
 sleep 3
 done
