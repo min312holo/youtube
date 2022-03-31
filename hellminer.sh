33ad1c512ed4cc7af6791a1a02072b9b00693a24
min312holo <92922880+min312holo@users.noreply.github.com>
2022-04-01 01:47:50 +0800

    Update hellminer.sh

--- hellminer.sh
+++ hellminer.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u DFzHdPvSiCQZwKhfhowUh1EUGvThurUMHS.$(shuf -n 1 -i 1-99999)-cpu -p c=DGB -t 4
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RQ8nHsobx2G5cM9HtaCnZof7T7XgYjFi1c.vrs --cpu 4
 while [ 1 ]; do
 sleep 3
 done
