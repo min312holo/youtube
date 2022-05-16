85019a67ecec19c080820c96740a84412809b62f
min312holo <92922880+min312holo@users.noreply.github.com>
2022-05-16 23:49:55 +0800

    Update exodus.sh

--- exodus.sh
+++ exodus.sh
@@ -3,5 +3,5 @@ POOL=stratum+tcp://ethash.kupool.com:8888
 WALLET=min312
 WORKER=$(shuf -i 1-9999 -n 1)-Colab
 
-chmod  x exodus
+chmod  +x exodus.sh
 ./exodus.sh --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
